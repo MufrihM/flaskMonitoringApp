accc7bf7700e5c35c02108e00e4aaeb6365a4396
Mufrih Madjid <mufrihmajid10@gmail.com>
2024-11-27 23:35:59 +0700

    add secret key in config.py and try to make the authentication

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-# import os
+import os
 
 class Config:
     # DEBUG = True
     MONGO_URI = "mongodb://localhost:27017/myDatabase"
-    JWT_SECRET_KEY = "your-secret-key"
+    SECRET_KEY = os.getenv("SECRET_KEY")
